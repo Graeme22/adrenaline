e79b180dcbbbe91bdca32517e31ffe00afe0329e
Hamza Abdalmajeed <hamza@Hamzas-MacBook-Pro-2.local>
2020-10-02 15:03:51 +0100

    Documentation + optimisation of the fitbit app

--- adrenaline-fitbit-app/companion/index.js
+++ adrenaline-fitbit-app/companion/index.js
@@ -1,24 +1,13 @@
-import * as messaging from "messaging";
+//Import required classes
+import * as messaging from "messaging"; // Messaging to allow for comunication between fitbit and companion app
 
-// Example POST method implementation:
-async function postData(url = '', data) {
-  console.log('hiiii')
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    headers: {
-      'Content-Type': 'text/plain'
-    },
-    body: data // body data type must match "Content-Type" header
-  });
-  return  response; // parses JSON response into native JavaScript objects
-}
-
-messaging.peerSocket.addEventListener("message", (evt) => {
-  console.log(evt.data)
-    postData('http://localhost:3000/heartrate', evt.data)
-    .then(data => {
-      console.log(data)
-    });
-  
+///Open Messaging connection
+messaging.peerSocket.addEventListener("message", (heartRate) => {
+  //When message is reccived send post request to nodejs server with the heartrate
+  fetch(
+    'http://localhost:3000/heartrate', //the URL for the request
+    {
+    method: 'POST', // The method
+    body: heartRate.data //the data to send to the server
+  })
 });
